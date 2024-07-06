e00461e3df0c35655eee3f0fbc035d8be1e29847
Soy Sauce <105949254+soysauce4444@users.noreply.github.com>
2024-07-06 19:05:16 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ontario Public Schools</title>
+  <title>Wuthering Waves</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
@@ -20,7 +20,7 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <h1 class="display-4 mt-5 mb-5">All Schools</h1>
+          <h1 class="display-4 mt-5 mb-5">All Characters and Compatible Weapons</h1>
         </div>
       </div>
     </div>
